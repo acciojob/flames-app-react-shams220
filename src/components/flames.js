5c6ef8135136b12830c296e36676489e961e53fd
shams220 <103259015+shams220@users.noreply.github.com>
2026-02-10 11:44:16 +0530

    new Changes

--- src/components/flames.js
+++ src/components/flames.js
@@ -5,79 +5,88 @@ const Flames = () => {
   const [name2, setName2] = useState("");
   const [result, setResult] = useState("");
 
-  const calculate = (name1, name2) => {
-    
-    let one = name1.toLowerCase();
-    let second = name2.toLowerCase();
-    let set1 = new Set(one);
-    let set2 = new Set(second);
-    let sizeSum = set1.size + set2.size;
-    if(set1.size == 0 || set2.size == 0){
-        setResult("Please Enter valid input")
-return
+  const calculate = () => {
+    // Blank input check
+    if (name1.trim() === "" || name2.trim() === "") {
+      setResult("Please Enter valid input");
+      return;
     }
 
-    // console.log("s1", set1.size);
-    // console.log("s2", set2.size);
+    let arr1 = name1.split("");
+    let arr2 = name2.split("");
 
-    let mod = Number(sizeSum) % 6;
-    // console.log("size",sizeSum)
-    // console.log("mod",mod)
-    // console.log(set1)
-    // console.log(set2)
-    // console.log(one ,second)
-    if (mod == 1) {
+    // Remove common letters (case-sensitive)
+    for (let i = 0; i < arr1.length; i++) {
+      let index = arr2.indexOf(arr1[i]);
+      if (index !== -1) {
+        arr1[i] = "";
+        arr2[index] = "";
+      }
+    }
+
+    let remainingLength =
+      arr1.join("").length + arr2.join("").length;
+
+    let mod = remainingLength % 6;
+
+    if (mod === 1) {
       setResult("Friends");
-    }else if(mod == 2){
-        setResult("Love")
-    }else if(mod == 3){
-        setResult("Affection")
-    }else if(mod == 4){
-        setResult("Marriage")
-    }else if(mod == 5){
-        setResult("Enemy")
-    }else{
-        setResult("Siblings");
+    } else if (mod === 2) {
+      setResult("Love");
+    } else if (mod === 3) {
+      setResult("Affection");
+    } else if (mod === 4) {
+      setResult("Marriage");
+    } else if (mod === 5) {
+      setResult("Enemy");
+    } else {
+      setResult("Siblings");
     }
   };
 
-  const clear=()=>{
-    setName1("")
-    setName2("")
-    setResult("")
-  }
+  const clear = () => {
+    setName1("");
+    setName2("");
+    setResult("");
+  };
+
   return (
     <div id="main">
-      {/* Do not remove the main div */}
       <input
         type="text"
+        data-testid="input1"
+        name="name1"
         value={name1}
         onChange={(e) => setName1(e.target.value)}
-        id="input1"
-        className="name1"
       />
 
       <input
         type="text"
+        data-testid="input2"
+        name="name2"
         value={name2}
         onChange={(e) => setName2(e.target.value)}
-        id="input2"
-        className="name2"
       />
 
       <button
-        className="calculate_relationship"
-        id="calculate_relationship"
-        onClick={() => calculate(name1, name2)}
+        data-testid="calculate_relationship"
+        name="calculate_relationship"
+        onClick={calculate}
       >
         Calculate Relationship Future
       </button>
-      <button id="clear" className="clear" onClick={clear}>
+
+      <button
+        data-testid="clear"
+        name="clear"
+        onClick={clear}
+      >
         Clear
       </button>
 
-      <h3 id="answer">{result}</h3>
+      <h3 data-testid="answer">{result}</h3>
     </div>
   );
 };
+
 export default Flames;
